aab0d6b8480a74b317dca0abc7cbdbfb7dfac94f
amrMoreno <adrianmorenoruiz@gmail.com>
2020-02-10 12:43:58 +0100

    Version1_IntroducionMapa
    Ya encontramos en el proyeto el mapa personjaes y otras cosas

---
+++ juegoFinal/core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,50 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class MyGdxGame extends ApplicationAdapter {
+	private OrthogonalTiledMapRenderer renderer; //Clase auxiliar para renderizar un mapa.
+	private OrthographicCamera camera; //Cámara a través de la que veremos el mundo.
+	private static int WIDTH; //Aquí almacenaremos la anchura en tiles
+	private static int HEIGHT; //Aquí almacenaremos la altura en tiles
+	public static final float unitScale = 1/16f; //Nos servirá para establecer que la pantalla se divide en tiles de 32 pixeles;
+	@Override
+	public void create () {
+		float w = Gdx.graphics.getWidth(); //Obtenemos la anchura de nuestra pantalla
+		float h = Gdx.graphics.getHeight(); //Obtenemos la atura de nuestra pantalla
+		TiledMap map = new TmxMapLoader().load("Mapa/mapa.tmx"); //Cargamos el tilemap desde assets
+		renderer = new OrthogonalTiledMapRenderer(map, unitScale); //Establecemos el renderizado del mapa dividido en Tiles de 16 dp.
+		camera = new OrthographicCamera(); //Declaramos la cámara a través de la que veremos el mundo
+		//camera.zoom=0.1f; //Establecemos el zoom de la cámara. 0.1 es más cercano que 1.
+		WIDTH = ((TiledMapTileLayer) map.getLayers().get(0)).getWidth(); //Obtenemos desde el mapa el número de tiles de ancho de la 1º Capa
+		HEIGHT = ((TiledMapTileLayer) map.getLayers().get(0)).getHeight(); //Obtenemos desde el mapa el número de tiles de alto de la 1º Capa
+		camera.setToOrtho(false, WIDTH,HEIGHT); //Establecemos la cámara, y le decimos cuanto tiene que ocupar. Doc:
+		camera.position.x=WIDTH/2;
+		camera.position.y=HEIGHT/2;
+		camera.zoom=1.001f;
+
+		camera.update();
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		renderer.setView(camera); //Establecemos la vista del mundo a través de la cámara.
+		renderer.render(); //Renderizamos la vista
+	}
+	
+	@Override
+	public void dispose () {
+		renderer.dispose(); //Destruimos el objeto que renderiza un mapa, para no tener filtraciones de memoria
+	}
+}
